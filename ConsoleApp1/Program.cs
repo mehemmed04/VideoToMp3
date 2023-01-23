f201e0f850cd678a12c33d5f169fc4119d8e3463
Bayr_kf60@itstep.edu.az <Bayr_kf60@itstep.edu.az>
2023-01-23 11:06:45 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = @"E:\Videos";
+            //var destination = @"E:\Musics";
+
+            string[] files = Directory.GetFiles(root);
+            foreach (var file in files)
+            {
+                var newfile = file.Replace(".vob", ".mp3");
+                newfile = newfile.Replace(".mpg", ".mp3");
+                newfile = newfile.Replace("Videos", "Musics");
+                var inputFile = new MediaFile { Filename = file };
+                var outputFile = new MediaFile { Filename = newfile };
+                using (var engine = new Engine())
+                {
+                    engine.GetMetadata(inputFile);
+                    engine.Convert(inputFile, outputFile);
+                }
+                Console.WriteLine(file + " converted succesfully to " + newfile);
+            }
+        }
+    }
+}
